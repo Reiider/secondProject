622004105453a2e5fa54125c70c7b7e0566e6060
Reiider <hapankoff@yandex.ru>
2016-10-30 12:59:20 +0300

    added json for meeting and their download

--- js/service/load.service.js
+++ js/service/load.service.js
@@ -4,9 +4,9 @@
   .module('service')
   .service('loadService', LoadService);
   
-  LoadService.$inject = ['$http', '$q', 'todayService', 'todoService'];
+  LoadService.$inject = ['$http', '$q', 'todayService', 'todoService', 'meetingService'];
   
-  function LoadService($http, $q, todayService, todoService){
+  function LoadService($http, $q, todayService, todoService, meetingService){
     var serv = this;
     serv.load = load;
 
@@ -15,16 +15,23 @@
       $http.get("json/"+random+".json").then(function(res){
           var data = res.data;
           todayService.setTodayDate(data.date);
-          return loadTodos(data.path);
+          return loadNextFile(data.path);
         }, function(){
           alert("error of loading first file");
         }
       ).then(function(res){
           var data = res.data;
           todoService.setElems(data.todos);
-          //return loadMeeting(data.path);
+          return loadNextFile(data.path);
         }, function(){
           alert("error of loading second file");
+        } 
+      ).then(function(res){
+          var data = res.data;
+          meetingService.setElems(data.meetings);
+          //return loadNextFile(data.path);
+        }, function(){
+          alert("error of loading third file");
         } 
       );
     }
@@ -33,7 +40,7 @@
       return Math.floor(Math.random() * (max - min + 1)) + min;
     }
     
-    function loadTodos(path){
+    function loadNextFile(path){
       return $http.get(path);
     }
     
