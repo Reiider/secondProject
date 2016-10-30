482076f013090f7b9c4fb5d28e75fb585368d820
Reiider <hapankoff@yandex.ru>
2016-10-30 12:35:37 +0300

    added meetingList, unsuccessful attempts to unit meeting and todo

---
+++ js/meeting/meeting.component.js
@@ -0,0 +1,24 @@
+(function(){
+
+  angular
+  .module('meeting')
+  .component('meetingList', {
+    templateUrl: 'template/meeting.template.html',
+    controller: Meeting
+  });
+  
+  Meeting.$inject = ['meetingService'];
+  
+  function Meeting(meetingService){
+    var vm = this;
+    vm.obj = meetingService.getObj();
+    vm.textAdd = "";
+    vm.timeAdd = ""
+    
+    vm.add = function(){meetingService.add(vm.textAdd, vm.timeAdd);}
+    vm.completeOrNot = function(index){meetingService.completeOrNot(index);}
+    vm.deleteSelectTodos = function(){meetingService.deleteSelectTodos();}
+  }
+  
+
+})();
---
+++ js/service/meeting.service.js
@@ -0,0 +1,64 @@
+(function(){
+
+  angular
+  .module('service')
+  .service('meetingService', MeetingService);
+  
+  function MeetingService(){
+    var serv = this;
+    
+    var obj = {};
+    obj.elems = [];
+    
+    this.setElems = setElems;
+    this.getObj = getObj;
+    this.add = add;
+    this.completeOrNot = completeOrNot;
+    this.deleteSelectTodos = deleteSelectTodos;
+    
+    function setElems(elements){
+      for(var i = 0; i < elements.length; i++){
+        obj.elems.push({
+          select: false, 
+          text: elements[i].text,
+          complete: false,
+          time: elements[i].time
+        });
+      }
+    }
+    
+    function getObj(){
+      return obj;
+    }
+    
+    function add(text, time){
+      if(text != "" && time != ""){
+        obj.elems.push({
+          select: false, 
+          text: text,
+          complete: false,
+          time: time
+        })
+      }
+    }
+    
+    function completeOrNot(index){
+      if(obj.elems[index].complete){
+        obj.elems[index].complete = false;
+      }
+      else{
+        obj.elems[index].complete = true;
+      }
+    }
+    
+    function deleteSelectTodos(){
+      for(var i = 0; i < obj.elems.length;){
+        if(obj.elems[i].select){
+          obj.elems.splice(i,1);
+        }
+        else i++;
+      }
+    }
+  }
+
+})();
